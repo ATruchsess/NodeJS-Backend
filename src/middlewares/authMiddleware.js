6ae7dbbd34caa90db2ee5d6df28bab94b37ee50b
ATruchsess <alexander.Truchsess@outlook.de>
2024-11-27 09:42:52 +0100

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -17,20 +17,13 @@ exports.authorization = (req, res, next) => {
   else {
     token = req.headers.authorization?.split(" ")[1];
   }
-
-  console.log(token);
-
+ 
   if (!token) {
     return res.status(401).json({ message: "Access Denied" });
   }
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET || "3f9c3e4b5a71cff8b241d49590c909f3c91ec3c460c9a9c9ebcdef51ba3d241e");
-    
     req.user = verified;
-    console.log(req.user);
-    console.log(req.user);
-    console.log(req.user);
-
     next();
   } catch (err) {
     res.status(400).json({ message: "Invalid Token" });
